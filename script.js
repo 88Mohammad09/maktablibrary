97d687503af18a921ec46986d891ee8004bced43
88Mohammad09 <mohammad.fallahpour2009@gmail.com>
2025-03-26 13:18:31 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    alert("به نمایشگاه مکتب خوش آمدید!\nبرای رای دادن به آثار به پایین صفحه مراجعه کنید:)");
+    console.log("صفحه بارگذاری شد");
+});
